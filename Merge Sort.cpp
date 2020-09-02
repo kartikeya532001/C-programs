8b13a543e7f804bef8fbdb31eaf79267c858f36a
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-09-03 00:02:07 +0700

    Merge Sort

---
+++ Merge Sort.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+// A function to merge the two half into a sorted data.
+void Merge(int *a, int low, int high, int mid)
+{
+	// We have low to mid and mid+1 to high already sorted.
+	int i, j, k, temp[high-low+1];
+	i = low;
+	k = 0;
+	j = mid + 1;
+ 
+	// Merge the two parts into temp[].
+	while (i <= mid && j <= high)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+ 
+	// Insert all the remaining values from i to mid into temp[].
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+ 
+	// Insert all the remaining values from j to high into temp[].
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+ 
+ 
+	// Assign sorted data stored in temp[] to a[].
+	for (i = low; i <= high; i++)
+	{
+		a[i] = temp[i-low];
+	}
+}
+ 
+// A function to split array into two parts.
+void MergeSort(int *a, int low, int high)
+{
+	int mid;
+	if (low < high)
+	{
+		mid=(low+high)/2;
+		// Split the data into two half.
+		MergeSort(a, low, mid);
+		MergeSort(a, mid+1, high);
+ 
+		// Merge them to get sorted output.
+		Merge(a, low, high, mid);
+	}
+}
+ 
+int main()
+{
+	int n, i;
+	int arr[]={53,52,39,70,65,31,34,67,22,45,26,46,32,44,87,33,500,55,42,93,39,25,89,100,123,5,6,600,36,66};
+	n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Size of array to be sorted is "<<n<<endl;
+	MergeSort(arr, 0, n-1);
+ 
+	// Printing the sorted data.
+	cout<<"\nSorted Data ";
+	for (i = 0; i < n; i++)
+        cout<<arr[i]<<" ";
+ 
+	return 0;
+}
